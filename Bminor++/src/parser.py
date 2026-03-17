989cf29ecfc8a96558cabe7d94527b1d5461071c
Pandi <pandiviris5@gmail.com>
2026-03-16 20:49:25 -0500

    Avance del AST hasta expr8 segun orden de la gramatica

--- Bminor++/src/parser.py
+++ Bminor++/src/parser.py
@@ -203,20 +203,20 @@ class Parser(sly.Parser):
 	# RETURN
 	@_("RETURN opt_expr ';'")
 	def return_stmt(self, p):
-		...
+		return ('RETURN', p.opt_expr)
 
 	@_("BREAK ';'")
 	def break_stmt(self, p):
-		...
+		return ('BREAK',)		# Se deja como tupla para facilitar acceso
 
 	@_("CONTINUE ';'")
 	def continue_stmt(self, p):
-		...
+		return ('CONTINUE',)	# Se deja como tupla para facilitar acceso
 
 	# BLOCK
 	@_("'{' stmt_list '}'")
 	def block_stmt(self, p):
-		...
+		return p.stmt_list
 		
 	# =================================================
 	# EXPRESIONES
@@ -224,27 +224,27 @@ class Parser(sly.Parser):
 	
 	@_("empty")
 	def opt_expr_list(self, p):
-		...
+		return []
 		
 	@_("expr_list")
 	def opt_expr_list(self, p):
-		...
+		return p.expr_list
 		
 	@_("expr ',' expr_list")
 	def expr_list(self, p):
-		...
+		return [p.expr] + p.expr_list
 		
 	@_("expr")
 	def expr_list(self, p):
-		...
+		return [p.expr]
 		
 	@_("empty")
 	def opt_expr(self, p):
-		...
+		return None
 		
 	@_("expr")
 	def opt_expr(self, p):
-		...
+		return [p.expr]
 		
 	# -------------------------------------------------
 	# PRIMARY
@@ -252,7 +252,7 @@ class Parser(sly.Parser):
 	
 	@_("expr1")
 	def expr(self, p):
-		...
+		return p.expr1
 		
 	@_("lval  '='  expr1")
 	@_("lval ADDEQ expr1")
@@ -261,21 +261,32 @@ class Parser(sly.Parser):
 	@_("lval DIVEQ expr1")
 	@_("lval MODEQ expr1")
 	def expr1(self, p):
-		...
+		return (p[1], p[0], p[2])
 		
 	@_("expr2")
+	@_("tern_op")
 	def expr1(self, p):
-		...
+		return p[0]
+	
+	# ----------- TERNARY OPERATOR -------------------
+
+	@_("expr2 '?' tern_op ':' tern_op")
+	def tern_op(self, p):
+		return ('TERNARY', p.expr2, p[2], p[4])
+	
+	@_("expr2")
+	def tern_op(self, p):
+		return p.expr2
 		
 	# ----------- LVALUES -------------------
 	
 	@_("ID")
 	def lval(self, p):
-		...
+		return ('ID', p.ID)
 		
 	@_("ID index")
 	def lval(self, p):
-		...
+		return ('ID_INDEX', p.ID, p.index)
 		
 	# -------------------------------------------------
 	# OPERADORES
@@ -283,19 +294,19 @@ class Parser(sly.Parser):
 	
 	@_("expr2 LOR expr3")
 	def expr2(self, p):
-		...
+		return (p.LOR, p.expr2, p.expr3)
 		
 	@_("expr3")
 	def expr2(self, p):
-		...
+		return p.expr3
 		
 	@_("expr3 LAND expr4")
 	def expr3(self, p):
-		...
+		return (p.LAND, p.expr3, p.expr4)
 		
 	@_("expr4")
 	def expr3(self, p):
-		...
+		return p.expr3
 		
 	@_("expr4 EQ expr5")
 	@_("expr4 NE expr5")
@@ -304,75 +315,60 @@ class Parser(sly.Parser):
 	@_("expr4 GT expr5")
 	@_("expr4 GE expr5")
 	def expr4(self, p):
-		...
+		return (p[1], p[0], p[2])
 
 	@_("expr5")
 	def expr4(self, p):
-		...
+		return p.expr5 
 		
 	@_("expr5 '+' expr6")
 	@_("expr5 '-' expr6")
 	def expr5(self, p):
-		...
+		return (p[1], p.expr5, p.expr6)
 		
 	@_("expr6")
 	def expr5(self, p):
-		...
+		return p.expr6
 		
 	@_("expr6 '*' expr7")
 	@_("expr6 '/' expr7")
 	@_("expr6 '%' expr7")
 	def expr6(self, p):
-		...
+		return (p[1], p.expr6, p.expr7)
 		
 	@_("expr7")
 	def expr6(self, p):
-		...
+		return p.expr7
 		
 	@_("expr7 '^' expr8")
 	def expr7(self, p):
-		...
+		return (p[1], p.expr7, p.expr8)
 		
 	@_("expr8")
 	def expr7(self, p):
-		...
+		return p.expr8
 		
 	@_("'-' expr8")
-	@_("'!' expr8")
+	@_("LNOT expr8")
+	@_("INC expr8")
+	@_("DEC expr8")
 	def expr8(self, p):
-		...
+		operador = p[0]
+		if operador == 'INC':
+			return ('PRE_INC', p.expr8)
+		elif operador == 'DEC':
+			return ('PRE_DEC', p.expr8)
+		else:
+			return (operador, p.expr8)
 
-	@_("expr9")
+	@_("expr9 INC")
+	@_("expr9 INC")
 	def expr8(self, p):
-		...
-
-	@_("postfix")
-	def expr9(self, p):
-		...
-
-	@_("primary")
-	def postfix(self, p):
-		...
-
-	@_("postfix INC")
-	def postfix(self, p):
-		...
-
-	@_("postfix DEC")
-	def postfix(self, p):
-		...
-
-	@_("prefix")
-	def primary(self, p):
-		...
-
-	@_("INC prefix")
-	def prefix(self, p):
-		...
-
-	@_("DEC prefix")
-	def prefix(self, p):
-		...
+		operador = p[1]
+		if operador == 'INC':
+			return ('POST_INC', p.expr9)
+		else:
+			return ('POST_DEC', p.expr9)
 
 	@_("group")
 	def prefix(self, p):
